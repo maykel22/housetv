d15d0d5b49bc1a11f54332f48f1b950f53ecb2f1
maykel22 <106408079+maykel22@users.noreply.github.com>
2022-10-26 20:27:13 -0500

    Create starRating.js

---
+++ js/v1.0.0.1/up/starRating.js
@@ -0,0 +1,38 @@
+var starRatingSystemSettings = {
+  "postPage" : {
+    "firebaseURL" : "https://housetv-project-default-rtdb.firebaseio.com/",
+    "fullStarImg" : " https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bxs-star.svg",
+    "emptyStarImg" : "https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bx-star.svg",
+    "hoverStarImg" : " https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bxs-star.svg",
+    "numberOfStars" : "5",
+    "starSize" : "20",
+    "textSize" : "15",
+    "textColor" : "#b8bab9",
+    "fontFamily" : "sf-regular",
+    "align" : "centro",
+    "topText" : "",
+    "bottomText" : "$average$ / $max$ ($votes$ votos)",
+    "thankYouText" : "Gracias por votar",
+    "blockingText" : "Ya has emitido tu voto, tu calificación es $userRating$.",
+ "position" : "top",
+    "status" : "activo"
+  },
+"indexPage" : {
+    "firebaseURL" : "https://housetv-project-default-rtdb.firebaseio.com/",
+    "fullStarImg" : " https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bxs-star.svg",
+    "emptyStarImg" : "https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bx-star.svg",
+    "hoverStarImg" : " https://cdn.rawgit.com/maykel22/housetv/e03ccb3eba011f5312c309fd68bc25271a406c78/stars%20rating/bxs-star.svg",
+    "numberOfStars" : "5",
+    "starSize" : "20",
+    "textSize" : "15",
+    "textColor" : "#b8bab9",
+    "fontFamily" : "sf-regular",
+    "align" : "centro",
+    "topText" : "",
+    "bottomText" : "$average$ / $max$ ($votes$ votos)",
+    "thankYouText" : "Gracias por votar",
+    "blockingText" : "Ya has emitido tu voto, tu calificación es $userRating$.",
+    "position" : "bottom",
+    "status" : "activo"
+  }
+}
